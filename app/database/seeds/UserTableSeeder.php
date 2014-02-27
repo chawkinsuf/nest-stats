2a79dc3df8c578bfdd4e05c88e7670fa7f3397dd
Chris Hawkins <chawkinsuf@gmail.com>
2014-02-27 04:23:11 +0000

    Update User with its namespace

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,7 +6,7 @@ class UserTableSeeder extends Seeder
 	public function run()
 	{
 		DB::table('users')->delete();
-		User::create(array(
+		App\Models\User::create(array(
 			'email'    => 'chawkinsuf@gmail.com',
 			'password' => '123456',
 			'nest_password' => '51gBJAh*yOEN*SK'
