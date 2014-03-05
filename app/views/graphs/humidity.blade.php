f1701caa5cf96c62bff10a4dd4e8152c54a2f8d6
Chris Hawkins <chawkinsuf@gmail.com>
2014-03-04 22:24:19 -0500

    Update humidity graph with temperature updates

--- app/views/graphs/humidity.blade.php
+++ app/views/graphs/humidity.blade.php
@@ -1,103 +1,195 @@
-<div id="humidity-graph"   class="highcharts-graph" style="height:600px;"></div>
+<div id="humidity-graph" class="highcharts-graph"></div>
 
 <script type="text/javascript">
 $(function() {
+	var chart = null;
+	var chartOptions = {
+		chart: {
+			renderTo: 'humidity-graph',
+			zoomType: 'x'
+		},
+		colors: [
+			'#2f7ed8',
+			'#0d233a',
+			'#8bbc21',
+			'#1aadce',
+			'#debd16',
+			'#910000'
+		],
+		title: {
+			text: 'Humidity Data',
+			align: 'left',
+			x: 20,
+			y: 25,
+			style: {
+				fontSize: '28px'
+			}
+		},
+		legend: {
+			width: 295,
+			shadow: true,
+			enabled: true,
+			itemDistance: 16,
+		},
+		rangeSelector : {
+			enabled: true,
+			selected : 0,
+			labelStyle: {
+				display: 'inline'
+			},
+			buttons: [
+				{ type: 'day', count: 1, text: '1d' },
+				{ type: 'day', count: 2, text: '2d' },
+				{ type: 'day', count: 3, text: '3d' },
+				{ type: 'day', count: 5, text: '5d' },
+				{ type: 'week', count: 1, text: '1w' },
+				{ type: 'week', count: 2, text: '2w' },
+				{ type: 'month', count: 1, text: '1m' },
+				{ type: 'all', count: 1, text: 'All' }
+			]
+		},
+		navigator: {
+			enabled: true
+		},
+		scrollbar : {
+			enabled : false
+		},
+		tooltip: {
+			formatter: function() {
+				var date = Highcharts.dateFormat( '%A, %b %e, %H:%M', this.x );
+				var s = '<span style="font-size:10px">'+ date +'</span>';
+				$.each( this.points, function( i, point ){
+					if ( /Humidity/.test(point.series.name) ){
+						s += '<br/><span style="fill:' + point.series.color + '">' + point.series.name + '</span><span>: </span>';
+						s += '<span style="font-weight:bold">' + point.y + '%</span>';
+					}
+				});
+				return s;
+			},
+			shared: true
+		},
+		yAxis: [{
+			title: {
+				text: 'Humidity (%)'
+			},
+			lineWidth: 2,
+			endOnTick: false,
+		},{
+			title: {
+				text: 'Status'
+			},
+			lineWidth: 2,
+			offset: 0,
+			max: 1.2,
+			tickInterval: 1,
+			gridLineWidth: 0,
+			showLastLabel: true,
+			startOnTick: false,
+			endOnTick: false,
+			labels: {
+				y: -5,
+				formatter: function(){
+					return this.value == 1 ? 'On' : 'Off';
+				}
+			}
+		}]
+	};
 
-	Highcharts.setOptions({
-		global : {
-			timezoneOffset: (new Date()).getTimezoneOffset()
+	var optionsSmall = {
+		optionsSet: true,
+		exporting: {
+			sourceWidth: 700,
+			sourceHeight: 400
+		},
+		legend: {
+			verticalAlign: 'bottom',
+			align: 'center',
+			floating: false,
+			x: 0,
+			y: 0
+		},
+		rangeSelector: {
+			inputEnabled: false
+		},
+		navigator: {
+			height: 20
+		},
+		yAxis: [{
+			height: 155,
+			startOnTick: false
+		},{
+			top: 250,
+			height: 40
+		}]
+	};
+
+	var optionsDefault = {
+		optionsSet: true,
+		exporting: {
+			sourceWidth: 1050,
+			sourceHeight: 600
+		},
+		legend: {
+			verticalAlign: 'top',
+			align: 'right',
+			floating: true,
+			x: -40,
+			y: -8
+		},
+		rangeSelector: {
+			inputEnabled: true
+		},
+		navigator: {
+			height: 40
+		},
+		yAxis: [{
+			height: 360,
+			startOnTick: true
+		},{
+			top: 455,
+			height: 60
+		}]
+	};
+
+	enquire.register('(max-width: 650px)', {
+		match : function() {
+
+			// If we already have a chart we need to use its options
+			if ( chart ){
+				chartOptions = chart.options;
+			}
+
+			// Set the new options
+			$.extend( true, chartOptions, optionsSmall );
+
+			// Recreate the chart
+			if ( chart ){
+				chart.destroy();
+				chart = new Highcharts.StockChart( chartOptions );
+			}
+		},
+		unmatch : function() {
+
+			// If we already have a chart we need to use its options
+			if ( chart ){
+				chartOptions = chart.options;
+			}
+
+			// Set the new options
+			$.extend( true, chartOptions, optionsDefault );
+
+			// Recreate the chart
+			if ( chart ){
+				chart.destroy();
+				chart = new Highcharts.StockChart( chartOptions );
+			}
 		}
 	});
 
-	var rangeButtons = [
-		{ type: 'day', count: 1, text: '1d' },
-		{ type: 'week', count: 1, text: '1w' },
-		{ type: 'week', count: 2, text: '2w' },
-		{ type: 'month', count: 1, text: '1m' }
-	];
+	$.getJSON('{{ URL::to('graphs/data') }}', function( data ){
 
-	$.getJSON('{{ URL::to('graphs/data') }}', function( data ) {
-
-		$('#humidity-graph').highcharts('StockChart', {
-
-			rangeSelector : {
-				selected : 1,
-				buttons: rangeButtons
-			},
-			colors: [
-				'#2f7ed8',
-				'#0d233a',
-				'#8bbc21',
-				'#1aadce',
-				'#debd16',
-				'#910000'
-			],
-			exporting: {
-				sourceWidth: 1000,
-				sourceHeight: 600
-			},
-			title: {
-				text: 'Humidity Data',
-				align: 'left',
-				x: 20,
-				y: 25,
-				style: {
-					fontSize: '28px'
-				}
-			},
-			legend: {
-				width: 260,
-				shadow: true,
-				enabled: true,
-				itemDistance: 16,
-				verticalAlign: 'top',
-				align: 'right',
-				floating: true,
-				x: -40,
-				y: -8
-			},
-			tooltip: {
-				formatter: function() {
-					var date = Highcharts.dateFormat( '%A, %b %e, %H:%M', this.x );
-					var s = '<span style="font-size:10px">'+ date +'</span>';
-					$.each( this.points, function( i, point ){
-						if ( /Humidity/.test(point.series.name) ){
-							s += '<br/><span style="fill:' + point.series.color + '">' + point.series.name + '</span><span>: </span>';
-							s += '<span style="font-weight:bold">' + point.y + '</span>';
-						}
-					});
-					return s;
-				},
-				shared: true
-			},
-			yAxis: [{
-				title: {
-					text: 'Humidity (%)'
-				},
-				height: 300,
-				lineWidth: 2,
-				endOnTick: false
-			},{
-				title: {
-					text: 'Status'
-				},
-				top: 400,
-				height: 100,
-				lineWidth: 2,
-				offset: 0,
-				max: 1.2,
-				tickInterval: 1,
-				gridLineWidth: 0,
-				showLastLabel: true,
-				startOnTick: false,
-				endOnTick: false,
-				labels: {
-					y: -5,
-					formatter: function(){
-						return this.value == 1 ? 'On' : 'Off';
-					}
-				}
-			}],
+		// Make our series data
+		var seriesData = {
 			series : [{
 				name: 'Inside Humidity',
 				yAxis: 0,
@@ -123,7 +215,18 @@ $(function() {
 				yAxis: 1,
 				data: data['alt_heat']
 			}]
-		});
+		};
+
+		// Add the data to our options
+		$.extend( true, chartOptions, seriesData );
+
+		// Add default options if needed
+		if ( ! chartOptions.optionsSet ){
+			$.extend( true, chartOptions, optionsDefault );
+		}
+
+		// Render the chart
+		chart = new Highcharts.StockChart( chartOptions );
 	});
 
 });
