0f32e3f8dfd5a507f245d2954e99682ffab8be11
Chris Hawkins <chawkinsuf@gmail.com>
2014-03-04 20:41:05 -0500

    Add global js options

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
 $(function(){
+
+	// Set global highcharts options
+	Highcharts.setOptions({
+		global : {
+			timezoneOffset: (new Date()).getTimezoneOffset()
+		}
+	});
+
+	// Set some alerify defaults
+	alertify.set({ delay: 20000 });
+
+	// Setup a click event for any spinner button
 	$('.has-spinner').click(function(){
 		$(this).prop('disabled', true);
 		$(this).toggleClass('spinning');
