6d5cd3ea51884718f36f40f632cc9635daf1ea26
Chris Hawkins <chawkinsuf@gmail.com>
2014-05-15 23:41:56 -0400

    Update the data collection cron

--- app/commands/NestData.php
+++ app/commands/NestData.php
@@ -4,8 +4,9 @@ namespace App\Commands;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Carbon\Carbon;
 
-use App\Models\User, App\Models\Device, App\Models\DataPoint;
+use App\Models\User, App\Models\Device, App\Models\Tracking, App\Models\DataSet, App\Models\DataPoint;
 use NestApi\Nest;
 
 class NestData extends Command {
@@ -22,7 +23,7 @@ class NestData extends Command {
 	 *
 	 * @var string
 	 */
-	protected $description = 'Command description.';
+	protected $description = 'Start record data from Nest devices.';
 
 	/**
 	 * Create a new command instance.
@@ -41,41 +42,96 @@ class NestData extends Command {
 	 */
 	public function fire()
 	{
-		$user = User::where( 'email', '=', 'chawkinsuf@gmail.com' )->first();
-		$nest = new Nest( $user->email, $user->nest_password );
-		$device = $user->devices()->first();
+		$dataInterval = 2;
+		$trackingDevices = Tracking::where( 'active', '=', '1' )->get();
 
 		$lastDate = -1;
 		while ( true ){
 
 			$date = date('i');
-			if ( $lastDate == $date || $date % 10 != 0 ){
-				sleep( 5 );
+			if ( $lastDate == $date || $date % $dataInterval != 0 ){
+
+				// Take a break
+				sleep( 10 );
+
+				// Update the list of devices we are tracking
+				$trackingDevices = Tracking::where( 'active', '=', '1' )->get();
+
+				// Move on
 				continue;
 			}
-
 			$lastDate = $date;
 
-			// Get the device information and store it
-			$ts = date('Y-m-d H:i:s');
-			$deviceData = $nest->getDeviceInfo( $device->serial );
-			$weatherData = $nest->getWeather( $device->postal );
+			foreach ( $trackingDevices as $trackingDevice ){
 
-			$dataPoint = new DataPoint;
-			$dataPoint->device_id = $device->id;
-			$dataPoint->temperature = $deviceData->current_state->temperature;
-			$dataPoint->outside_temperature = $weatherData->outside_temperature;
-			$dataPoint->humidity = $deviceData->current_state->humidity;
-			$dataPoint->outside_humidity = $weatherData->outside_humidity;
-			$dataPoint->target_temperature = is_array( $deviceData->target->temperature ) ? $deviceData->target->temperature[1] : $deviceData->target->temperature;
-			$dataPoint->ac = $deviceData->current_state->ac;
-			$dataPoint->heat = $deviceData->current_state->heat;
-			$dataPoint->alt_heat = $deviceData->current_state->alt_heat;
-			$dataPoint->fan = $deviceData->current_state->fan;
-			$dataPoint->date = $ts;
-			$dataPoint->save();
+				// Setup some variables
+				$dataSet = $trackingDevice->data_set;
+				$device = $trackingDevice->device;
+				$user = $device->user;
 
-			var_dump( $dataPoint->toArray() );
+				// Open the nest connection
+				$nest = new Nest( $user->email, $user->nest_password );
+
+				// If we don't have a data set or our data interval is out of whack, we need a new data set
+				if ( $dataSet === null || ( $dataSet->last_data !== null && Carbon::now()->diffInMinutes( $dataSet->last_data->date ) > $dataInterval ) ){
+					$dataSet = new DataSet;
+					$trackingDevice->device->dataSets()->save( $dataSet );
+					$trackingDevice->dataSet()->associate( $dataSet );
+					$trackingDevice->save();
+				}
+
+				print "\nNew Data Point\n";
+				$ts = date('Y-m-d H:i:s');
+
+				// Get the device and weather information
+				$deviceData = $nest->getDeviceInfo( $device->serial );
+				$weatherData = $nest->getWeather( $device->postal );
+				//var_dump( $deviceData );
+
+				// Handle different variations for the target temperature
+				switch ( $deviceData->target->mode ){
+					case 'range':
+						$target_heat = $deviceData->target->temperature[0];
+						$target_cool = $deviceData->target->temperature[1];
+						break;
+					case 'heat':
+						$target_heat = $deviceData->target->temperature;
+						$target_cool = null;
+						break;
+					case 'cool':
+						$target_heat = null;
+						$target_cool = $deviceData->target->temperature;
+						break;
+					default:
+						$target_heat = null;
+						$target_cool = null;
+				}
+
+				// Make the data record
+				$dataPoint = new DataPoint;
+				$dataPoint->data_set_id = $dataSet->id;
+				$dataPoint->temperature = $deviceData->current_state->temperature;
+				$dataPoint->outside_temperature = $weatherData->outside_temperature;
+				$dataPoint->humidity = $deviceData->current_state->humidity;
+				$dataPoint->outside_humidity = $weatherData->outside_humidity;
+				$dataPoint->target_heat = $target_heat;
+				$dataPoint->target_cool = $target_cool;
+				$dataPoint->ac = $deviceData->current_state->ac;
+				$dataPoint->heat = $deviceData->current_state->heat;
+				$dataPoint->aux_heat = $deviceData->current_state->alt_heat;
+				$dataPoint->fan = $deviceData->current_state->fan;
+				$dataPoint->date = $ts;
+				$dataPoint->save();
+
+				// Update the data set
+				$dataSet->lastData()->associate( $dataPoint );
+				if ( $dataSet->first_data === null ){
+					$dataSet->firstData()->associate( $dataPoint );
+				}
+				$dataSet->save();
+
+				var_dump( $dataPoint->toArray() );
+			}
 		}
 	}
 
